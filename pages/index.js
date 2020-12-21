1766a851c6d5aff31ef7d71bae690789be5787b9
mjabdi <m.jafarabdi@gmail.com>
2020-12-21 14:18:10 +0330

    add some space

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,16 @@ export default function Home() {
 
       <main className={styles.main}>
 
-      <h1 className={styles.title}>
-        به اینیسا شاپ خوش آمدید
-      </h1>
+
+
+
+
+      <div style={{margin:"20px"}}>
+        <h1 className={styles.title}>
+          به اینیسا شاپ خوش آمدید
+        </h1>
+
+      </div>
 
 
         <h1 className={styles.title}>
