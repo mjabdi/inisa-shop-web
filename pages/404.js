b2c89b9cf378ba0e3e2c2314ea4822348e9d41d9
mjabdi <m.jafarabdi@gmail.com>
2020-12-21 23:02:30 +0330

    custom 404 message

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,7 @@
-import { i18n, withTranslation } from '../i18n'
-import PropTypes from 'prop-types'
 
-const Custom404 = ({t}) => {
-    return <h1 style={{padding:"20px"}}> {t('pageNotFound')} </h1>
+
+const Custom404 = () => {
+    return <h1 style={{padding:"20px"}}> {'صفحه مورد نظر پیدا نشد!'} </h1>
   }
 
-  Custom404.getInitialProps = async () => ({
-    namespacesRequired: ['common'],
-  })
-  
-  Custom404.propTypes = {
-    t: PropTypes.func.isRequired,
-  }
-  
-  export default withTranslation('common')(Custom404)
+  export default Custom404
