3d7f7fb90c31d94ef38d551cfdf85f2de0d62932
salikf <skorooni@yahoo.com>
2021-01-08 00:53:41 +0330

    new layyout

--- components/home/app-bar.js
+++ components/home/app-bar.js
@@ -11,6 +11,9 @@ import { useScrollTrigger } from '@material-ui/core';
 import React from 'react';
 import Fade from '@material-ui/core/Fade';
 import Slide from '@material-ui/core/Slide';
+import Grid from '@material-ui/core/Grid'
+import SearchIcon from '@material-ui/icons/Search';
+import Image from 'next/image';
 
 function ChangeOnScroll(props) {
   const { children, window } = props;
@@ -23,34 +26,34 @@ function ChangeOnScroll(props) {
     target: window ? window() : undefined,
   });
 
-  
-    return (
 
-          !trigger ?
-            (
-              React.cloneElement(children, {
-                elevation: 0,
-                color : 'transparent'
-              })
+  return (
 
-            ) :
-            (
-              <Slide direction="down" timeout={1000} in={true} mountOnEnter unmountOnExit>
-              {
-                React.cloneElement(children, {
-                  // elevation: 4,
-                  style: {opacity: 0.95, backgroundColor: "#fefefe", border: 0, boxShadow: "0 8px 20px 0 rgba(0, 0, 0, 0.1)"}
-                })
-               
-                }
-                </Slide>
-         
-            )
-          
-    );
-    
-   
-  
+    !trigger ?
+      (
+        React.cloneElement(children, {
+          elevation: 0,
+          color: 'transparent'
+        })
+
+      ) :
+      (
+        <Slide direction="down" timeout={1000} in={true} mountOnEnter unmountOnExit>
+          {
+            React.cloneElement(children, {
+              // elevation: 4,
+              style: { opacity: 0.95, backgroundColor: "#fefefe", border: 0, boxShadow: "0 8px 20px 0 rgba(0, 0, 0, 0.1)" }
+            })
+
+          }
+        </Slide>
+
+      )
+
+  );
+
+
+
 }
 
 ChangeOnScroll.propTypes = {
@@ -62,16 +65,103 @@ ChangeOnScroll.propTypes = {
   window: PropTypes.func,
 };
 
-export default function MyAppBar ({t, ...props}) 
-{
-    return (
+export default function MyAppBar({ t, ...props }) {
+  return (
 
-      <div className={styles.root}>
-        <ChangeOnScroll {...props}>
+    <div className={styles.root}>
+      <ChangeOnScroll {...props}>
         <AppBar>
-          <Toolbar>
+          <Toolbar style={{color:"#000" , fontWeight:"500"}}>
+            <Grid
+              container
+              id="appbar"
+              direction="row-reverse"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} md={6} id="logo" >
+                <Grid
+                  container
+                  id="logoside"
+                  direction="row-reverse"
+                  justify="flex-start"
+                  alignItems="center"
+                  spacing={4}
+                >
+                  
+                  <Grid item id="logoside0" >
+                    <div style={{ width: "30px" }}></div>
+                  </Grid>
 
-            <div className={styles.title}>
+                  <Grid item id="logoside1" >
+                    <Grid container justify="center" alignItems="flex-end" direction="row-reverse" spacing={1}>
+                      <Grid item>
+                          <div className={styles.logoContainer}>
+                            <Image src="/images/logo_only.png" width="40px" height="40px" alt="logo"/>
+                          </div>
+                      </Grid>
+                      <Grid item>
+                            <span style={{fontSize:"1.5rem", fontWeight:"800"}}> Inisashop </span>
+                      </Grid>
+                    </Grid>
+
+                   
+                  </Grid>
+
+                  <Grid item id="logoside2" >
+                    <div style={{ width: "50px" }}></div>
+                  </Grid>
+                  <Grid item id="logoside3" >
+                    <span>درباره اینیسا</span>
+                  </Grid>
+                  <Grid item id="logoside4" >
+                    <span>پکیجینگ</span>
+                  </Grid>
+
+                </Grid>
+              </Grid>
+              <Grid item xs={12} md={6} id="startbutton" >
+                <Grid
+                  container
+                  id="buttonside"
+                  direction="row-reverse"
+                  justify="flex-end"
+                  alignItems="center"
+                  spacing={4}
+                >
+
+                  <Grid item id="buttonside1" >
+                    <span>راهنما</span>
+                  </Grid>
+                  <Grid item id="buttonside2" >
+                    <span>بلاگ</span>
+                  </Grid>
+                  <Grid item id="buttonside3" >
+                    <IconButton>
+                        <SearchIcon/>
+                    </IconButton>
+                  </Grid>
+                  <Grid item id="buttonside4" >
+                    <span>ورود</span>
+                  </Grid>
+                  <Grid item id="buttonside5" >
+                    <Button
+                      style={{ backgroundColor: "#111111", color: "#fff" }}
+                      type="button"
+                      variant="contained"
+                    >
+                      رایگان فروشگاه بساز
+                                    </Button>
+                  </Grid>
+                  <Grid item id="buttonside6" >
+                    <div style={{ width: "30px" }}></div>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+            </Grid>
+
+            {/* <div className={styles.title}>
               <span>  {t('inisa')} </span>
               <span className={styles.yellow}>  {t('shop')} </span>
               
@@ -82,12 +172,12 @@ export default function MyAppBar ({t, ...props})
             >
 
               {t('login')}
-            </Button>
+            </Button> */}
           </Toolbar>
         </AppBar>
-        </ChangeOnScroll>
-      </div>
-    );
+      </ChangeOnScroll>
+    </div>
+  );
 }
 
 // MyAppBar.getInitialProps = async () => ({
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import AppBar from '../components/home/app-bar'
 import { i18n, withTranslation } from '../i18n'
 import PropTypes from 'prop-types'
@@ -10,68 +10,149 @@ import { Typography } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
 import FacilityCard from '../components/home/facility-card'
 import SectionCard from '../components/home/section-card'
-
-
-const facilityCards = [
-  { name: 'فروش', desc: '  امکان فروش محصولات به سادگی در این سایت برای شما فراهم شده است، با ثبت نام در سایت می توانید به تمامی محصولات اینستاگرام خود دسترسی داشته باشید.' },
-  { name: 'خرید', desc: 'خریداران محترم میتوانند به راحتی چند کلیک محصول مورد علاقه خود را که به صورت هشتگ های اینستاگرام موجود می باشد، جستجو و پیدا کنند.' },
-  { name: 'سبد خرید', desc: 'بدون نیاز به ارسال پیام بین فروشنده و خریدار، با ثبت کالا در سبد خرید از فروشگاه های اینستاگرام خرید کنید' },
-  { name: 'خرید آنلاین', desc: 'خرید خود را با پرداخت آنلاین با اطمینان و پشتوانه این سایت انجام دهید.' },
-
-]
-
-const sectionCards = [
-  { name: 'فروشنده',img:"/images/sellersec.jpg", desc: 'تمامی محصولات ارائه شده در صفحه اینستاگرام خود را با ثبت نام در بخش فروشندگان قابل ارائه برای فروش آنلاین درآورید' },
-  { name: 'خریدار',img:"/images/buyersec.jpg", desc: 'محصولات مورد نظر خود را از صفحه دلخواه خود در اینستاگرام به راحتی با ثبت نام در این سایت به صورت آنلاین خریداری نمایید' },
-  { name: 'بلاگ',img:"/images/blogsec.jpg", desc: 'در این بخش مطالبی جذاب و آموزنده در خصوص مد، استایل، ترکیب بندی لباس، دکور، طراحی داخلی، و هر آنچه برای یک زندگی شیرین مورد نیاز است ارائه می گردد.'},
-  { name: 'رویدادها',img:"/images/eventsec.jpg", desc: 'رویدادهای مهم سال، جشن ها، سال نو، حراج ها، را به بهترین نحو برگزار می کنیم' },
-  { name: 'برندینگ',img:"/images/brandsec.jpg", desc: 'با ما محصولات خود را برند کنید، و یا برند دلخواه خود را ایجاد کنید'},
-  { name: 'ارتباط با ما',img:"/images/contactsec.jpg", desc: 'با ما همراه باشید، پیشنهادات و انتقادات شما راهیست به سوی موفقیت ' },
-]
-
-
-const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2 - 50]
-const trans1 = (x, y) => `translate3d(${x / 5}px,${y / 5}px,0)`
-// const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
-// const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
-// const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
-
-
-const AnimFeTurbulence = animated('feTurbulence')
-const AnimFeDisplacementMap = animated('feDisplacementMap')
-
+import clx from 'classnames'
+import Button from '@material-ui/core/Button';
+import { FormatColorText, FormatUnderlined, Rotate90DegreesCcw } from '@material-ui/icons'
 
 
 const Home = ({ t }) => {
 
+  const sloganArray = [
+    {title: 'فروش آنلاین محصول', bg: styles.hpc_underlined_yellow},
+    {title: 'اتصال به اینستاگرام', bg: styles.hpc_underlined_magenta},
+    {title: 'فروش روی وب سایت', bg: styles.hpc_underlined_green},
+    {title: 'راه اندازی کسب و کار', bg: styles.hpc_underlined_indigo},
+  ]
+
+  const hpc_pics_bg = [
+    styles.hpc_pics_bg_yellow,
+    styles.hpc_pics_bg_magenta,
+    styles.hpc_pics_bg_green,
+    styles.hpc_pics_bg_indigo,
+
+  ]
+
+  const [sloganArrayIndex, setSloganArrayIndex] = React.useState(0)
+  const [sloganIndex, setSloganIndex] = React.useState(0)
+  const [hpcAnimationClasses, setHpcAnimationClasses] = React.useState(clx(styles.hpc_animate))
+  const [hpcAnimationClasses2, setHpcAnimationClasses2] = React.useState(clx(styles.hpc_animate))
+  const [hpcAnimationClasses3, setHpcAnimationClasses3] = React.useState(clx(styles.hpc_animate))
+
+  const [hpc_pics_degree, setHpc_piscs_degree] = React.useState(30)
+  const [hpc_pics_x, setHpc_piscs_x] = React.useState(0)
+  const [hpc_pics_y, setHpc_piscs_y] = React.useState(0)
+
+  const [hpc_phone_degree, setHpc_phone_degree] = React.useState(30)
+  const [hpc_phone_x, setHpc_phone_x] = React.useState(0)
+  const [hpc_phone_y, setHpc_phone_y] = React.useState(0)
+
+  
+  const [hpc_tablet_degree, setHpc_tablet_degree] = React.useState(30)
+  const [hpc_tablet_x, setHpc_tablet_x] = React.useState(0)
+  const [hpc_tablet_y, setHpc_tablet_y] = React.useState(0)
+
+ 
   React.useEffect(() => {
     i18n.changeLanguage('fa');
-    const interval = setInterval(() => {
-      setBgX(prevX => prevX - 1);
-    }, 10);
 
-    return () =>
-    {
-      clearInterval(interval);
+
+    const sloganTimer = setInterval(() => {
+      setSloganIndex(prev => {
+        if ( prev < (sloganArray[sloganArrayIndex].title.length * 2.2))
+        {
+          return prev + 1
+        }
+        else
+        {
+          setSloganArrayIndex(prev => prev < sloganArray.length - 1 ? prev + 1 : 0)
+          return 0
+        }
+       
+      })
+      
+    }, 120);
+
+    setHpcAnimationClasses(clx(styles.hpc_animate_from_left,styles.hpc_animate_delay_1 ))
+    setHpcAnimationClasses2(clx(styles.hpc_animate_from_left,styles.hpc_animate_delay_2 ))
+    setHpcAnimationClasses3(clx(styles.hpc_animate_from_left,styles.hpc_animate_delay_3 ))
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      clearInterval(sloganTimer)
+      window.removeEventListener('scroll', handleScroll);
     }
 
   }, []);
 
-  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
 
-  const [bgX, setBgX] = React.useState(0);
 
-  const [open, toggle] = useState(false)
-  const { freq, scale, transform, opacity } = useSpring({
-    reverse: open,
-    from: { scale: 10, opacity: 0, transform: 'scale(0.9)', freq: '0.0175, 0.0' },
-    to: { scale: 150, opacity: 1, transform: 'scale(1)', freq: '0.0, 0.0' },
-    config: { duration: 3000 }
-  })
-
-  const getBgX = () =>
+  const handleScroll = (event) =>
   {
+    const scrollTop = parseInt((window.scrollY / window.innerHeight) * 100)
+    let degree = 30 - scrollTop
+    if (degree < 0)
+      degree = 0
+    setHpc_piscs_degree(degree)
+    setHpc_piscs_x(scrollTop * 8)
+    setHpc_piscs_y(-scrollTop * 5)
 
+    degree = (30 - scrollTop / 2) 
+    if (degree > 30)
+      degree = 30
+
+    if (degree < 0)
+      degree = 0
+
+    setHpc_phone_degree(degree)
+    let x = scrollTop * 4
+    let y = scrollTop * 8
+    if (x > 220)
+      x = 220
+    if (y > 450)
+      y = 450  
+    setHpc_phone_x(x)
+    setHpc_phone_y(y)
+
+    setHpc_tablet_degree(degree)
+     let x2 = scrollTop * 7
+     let y2 = scrollTop * 1.5
+
+     if (x2 > 400)
+     {
+       x2 = 400
+     }
+     if (y2 > 50)
+     {
+       y2 = 50
+     }
+
+      setHpc_tablet_x(x2)
+      setHpc_tablet_y(y2)
+    
+     
+
+
+
+
+
+  }
+
+  
+  const getCurrentSlogan = () =>
+  {
+    return sloganArray[getCurrentSloganIndex()]
+  }
+
+  const getCurrentSloganIndex = () =>
+  {
+    return hpc_pics_degree < 25 ?  0 : sloganArrayIndex
+  }
+
+
+  const getSloganPart = () => {
+
+    return getCurrentSlogan().title.substr(0,sloganIndex)
   }
 
 
@@ -82,13 +163,193 @@ const Home = ({ t }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <AppBar t={t} />
 
-      <main className={styles.main}>
+      <div className={styles.container}>
 
-        <AppBar t={t} />
+      <Grid
+        container
+        id="sellerhome"
+        direction="column"
+        justify="center"
+        alignItems="stretch"
+      >
+        <Grid item xs={12} id="1" >
+          <div className={styles.grid1}>
+            <Grid
+              container
+              id="sec1"
+              direction="row-reverse"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec11" xs={12} md={6}>
+                <div className={styles.sec11}>
+                  <h1 className={hpcAnimationClasses}>
+                    <div>همین حالا آغاز کنید</div>
+                    <div>
+                   
+                        <span className={clx(styles.hpc_underlined,getCurrentSlogan().bg)}>{getSloganPart()}</span>
+                        <span className={clx(styles.hpc_caret,styles.hpc_caret_show,styles.hpc_caret_blink)}></span>
+                
+                    </div>
+                  </h1>
+                  <p className={hpcAnimationClasses2}>
+                    با ما فروش آنلاین را تجربه کنید. با اینیسا، فروشگاه رایگان خود را برای همیشه داشته باشید. یکبار ثبت نام کنید و همیشه استفاده کنید.
+                  </p>
+                  <div className={hpcAnimationClasses3}>
+                    <Button
+                        style={{ backgroundColor: "#111111", color: "#fff", width: "200px", padding: "10px", marginRight:"250px" }}
+                        type="button"
+                        variant="contained"
+                      >
+                        رایگان فروشگاه بساز
+                      </Button>
+                  </div>
+                </div>
+              </Grid>
+              <Grid item id="sec12" xs={12} md={6}>
 
- 
-      </main>
+              
+                <div 
+                  className={clx(styles.hpc_phone__cover)} 
+                  style={{transform:`rotate(${hpc_phone_degree}deg) translateX(${hpc_phone_x}px) translateY(${hpc_phone_y}px)`}}> 
+                   <Image className={clx(styles.hpc_phone__slide_current)}  src="/images/phone-slide1_.jpg" width="250px" height="450px" alt="phone-pic"/>
+                </div>
+
+                {/* <div className={clx(styles.hpc_phone__slide_current)} >
+                    <Image src="/images/phone-slide1_.jpg" width="348px" height="556px" alt="phone-pic"/>
+                 </div> */}
+
+                <div 
+                  className={clx(styles.hpc_tablet__cover)} 
+                  style={{transform:`rotate(${hpc_tablet_degree}deg) translateX(${hpc_tablet_x}px) translateY(${hpc_tablet_y}px)`}}> 
+                </div>
+
+              
+
+                <div className={clx(styles.hpc_pics_container)} style={{transform:`rotate(${hpc_pics_degree}deg) translateX(${hpc_pics_x}px) translateY(${hpc_pics_y}px)`}}>
+                   <div className={clx(styles.hpc_pics,hpc_pics_bg[getCurrentSloganIndex()])}>   </div>
+                </div>
+
+
+                  
+                
+              
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="2" >
+          <div className={styles.grid2}>
+            <Grid
+              container
+              id="sec2"
+              direction="row"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec21" xs={12} md={6}>
+                <div className={styles.sec21}></div>
+              </Grid>
+              <Grid item id="sec22" xs={12} md={6}>
+                <div className={styles.sec22}></div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={12} id="3" >
+          <div className={styles.grid3}>
+            <Grid
+              container
+              id="sec3"
+              direction="row"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec31" xs={12} md={6}>
+                <div className={styles.sec31}></div>
+              </Grid>
+              <Grid item id="sec32" xs={12} md={6}>
+                <div className={styles.sec32}></div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="4" >
+          <div className={styles.grid4}>
+            <Grid
+              container
+              id="sec4"
+              direction="row"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec41" xs={12} md={6}>
+                <div className={styles.sec41}></div>
+              </Grid>
+              <Grid item id="sec42" xs={12} md={6}>
+                <div className={styles.sec42}></div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="5" >
+          <div className={styles.grid5}>
+            <Grid
+              container
+              id="sec5"
+              direction="row"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec51" xs={12} md={6}>
+                <div className={styles.sec51}></div>
+              </Grid>
+              <Grid item id="sec52" xs={12} md={6}>
+                <div className={styles.sec52}></div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="6" >
+          <div className={styles.grid6}>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="7" >
+          <div className={styles.grid7}>
+            <Grid
+              container
+              id="sec7"
+              direction="row"
+              justify="center"
+              alignItems="center"
+              
+            >
+              <Grid item id="sec71" xs={12} md={4}>
+                <div className={styles.sec71}></div>
+              </Grid>
+              <Grid item id="sec72" xs={12} md={4}>
+                <div className={styles.sec72}></div>
+              </Grid>
+              <Grid item id="sec73" xs={12} md={4}>
+                <div className={styles.sec73}></div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={12} id="8" >
+          <div className={styles.grid8}>
+          </div>
+        </Grid>
+      </Grid>
+     
+     </div>
 
       <footer className={styles.footer}>
         <a
